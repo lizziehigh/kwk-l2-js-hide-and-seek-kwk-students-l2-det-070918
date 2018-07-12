26e91a1c9d69a7570c5f9353418ac995c801fbca
lizziehigh <lizziehigh02@gmail.com>
2018-07-12 18:10:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')  
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll(".ranked-list")
+  
+  for (let i = 0; i < rankedList.length; i++) {
+    let children = rankedList[i].children
+    
+  
+  
+  for (let j = 0, k = children.length; j <  k; j++) {
+    children[j].innerHTML = parseInt(children[j].innerHTML) + n
+  }
+}
+}
